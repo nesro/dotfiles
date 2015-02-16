d6c0953d0f5892d0bf1590b655f9b971e3fcb3c8
nesro <nesro@nesro.cz>
2015-02-16 12:53:18 +0100

    better backup

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ backup_and_link()
 	file=$1
 
 	if [[ -f ~/$file ]]; then
-		mv ~/$file{,_backup}
+		echo "$file already exists. creating a backup"
+		mv ~/$file{,_backup_$(date +"%y_%m_%d")}
 	fi
 
 	ln -s $(readlink -f $file) ~/$file
-
 }
 
-backup_and_link ".tmux.conf"
+backup_and_link ".vimrc"
 
